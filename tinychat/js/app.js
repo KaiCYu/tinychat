7428b1c8443055c4d2530ca80a5137afbab63f39
KaiCYu <kaichyu@gmail.com>
2017-08-22 22:45:04 -0700

    displayed JSON on page Please enter the commit message for your changes. Lines starting

--- tinychat/js/app.js
+++ tinychat/js/app.js
@@ -1,15 +1,34 @@
 /* Stub JS file for your tinychat app! */
 
 $(document).ready(function(){
+    
     // Stub AJAX call that demos getting the fixture data
     $.getJSON('/fixtures/fakedata.json', function(data) {
         console.log("success");
-        console.log(data);
+        // console.log(data);
+
+        let messages = data.messages;
+        let messageArea = $('.message-area');
+
+        // console.log(messages);
+        messages.forEach(message => {
+            let element = document.createElement("div");
+            element.innerHTML = message.content;
+            // console.log('message area', messageArea);
+            console.log('element', element);
+            messageArea.append(element);
+        })
+
+
     }).done(function() {
         console.log("another success message");
+
     }).fail(function() {
         console.error("error");
     }).always(function() {
         console.info("complete");
+        //check for new messages, update messages
     });
+
+
 });
